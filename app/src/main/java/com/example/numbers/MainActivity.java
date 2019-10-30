d4a1117b32837b5b0abe05b7f1f7e0d027f0166c
miia <miia@com.tw>
2019-10-31 02:20:29 +0800

    新增圖片和猜數字

--- app/src/main/java/com/example/numbers/MainActivity.java
+++ app/src/main/java/com/example/numbers/MainActivity.java
@@ -8,28 +8,47 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    String TAG = MainActivity.class.getSimpleName();
     private TextView number;
     int counter;
+    int secret = new Random().nextInt(10)+1;
+    private ImageView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Log.d(TAG,"secret:"+secret);
         number = findViewById(R.id.num);
+        result = findViewById(R.id.result_image);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 counter++;
                 number.setText(String.valueOf(counter));
+                result.setAlpha(1.0f);
+                result.setVisibility(view.VISIBLE);
+                if(counter == secret){
+                    Toast.makeText(MainActivity.this,"You got it!",Toast.LENGTH_LONG).show();
+                    result.setImageResource(R.drawable.angry);
+                }else{
+                    result.setImageResource(R.drawable.happy);
+                    result.animate().alpha(0.0f).setDuration(1200);
+                }
             }
         });
     }
